08ed0e5f7ad248b9be5719a7eb068c9363f829f2
Scott <1009332628@qq.com>
2024-07-29 23:53:32 +0800

    features: add tower-http support

--- src/process/http_serve.rs
+++ src/process/http_serve.rs
@@ -6,6 +6,7 @@ use axum::{
     Router,
 };
 use std::{net::SocketAddr, path::PathBuf, sync::Arc};
+use tower_http::services::ServeDir;
 use tracing::{info, warn};
 
 #[derive(Debug)]
@@ -21,9 +22,17 @@ pub async fn process_http_serve(path: PathBuf, port: u16) -> Result<()> {
     info!("Serveing {:?} on port{},", path, addr);
 
     // 将启动命令时传入的path交给state
-    let state = HttpServeState { path };
+    let state = HttpServeState { path: path.clone() };
+
+    let service = ServeDir::new(path)
+        .append_index_html_on_directories(true)
+        .precompressed_gzip()
+        .precompressed_br()
+        .precompressed_deflate()
+        .precompressed_zstd(); // 开启几个压缩方式的开关
 
     let router = Router::new()
+        .nest_service("/tower", service)
         .route("/*path", get(file_handler))
         .route("/", get(hello_world_handler))
         .with_state(Arc::new(state)); // 多个线程中，都会对这个内存指定，只有进程结束，或者指针为0时， state才会被释放掉
@@ -62,5 +71,24 @@ async fn file_handler(
             StatusCode::NOT_FOUND,
             format!("404 Not Found: {:?}", file_path.display()),
         )
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_file_handler() {
+        let state = Arc::new(HttpServeState {
+            path: PathBuf::from("."),
+        });
+
+        let (statue, content) = file_handler(State(state), Path("Cargo.toml".to_string())).await;
+        println!("read file: {:?}", content);
+        assert_eq!(statue, StatusCode::OK);
+        assert!(content.starts_with("[package]"));
+        // run this test use nextest show detail
+        // cargo nextest run -- test_file_handler
     }
 }
